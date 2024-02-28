1c0e980ae5d221090533c1c9e4d2a1074f0142ec
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-02-28 09:23:37 +0400

    entity fixed

--- src/main/java/az/code/agency/entity/Request.java
+++ src/main/java/az/code/agency/entity/Request.java
@@ -1,14 +1,13 @@
 package az.code.agency.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -33,6 +32,8 @@ public class Request {
     @Convert(converter = StringToJsonConverter.class)
     private Map<String, Object> answers;
 
-    @Enumerated(EnumType.STRING)
-    RequestStatus status;
+    @ManyToOne
+    @JsonIgnore
+    @ToString.Exclude
+    Offer offer;
 }
---
+++ src/main/java/az/code/agency/entity/Status.java
@@ -0,0 +1,5 @@
+package az.code.agency.entity;
+
+public enum Status {
+    PENDING,ANSWERED, ARCHIVED,ASSIGNED
+}
